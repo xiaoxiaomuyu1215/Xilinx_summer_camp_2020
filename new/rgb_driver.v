11c80ed35ac82a3a9fcd893d1dcbbe4441c285b5
xiaoxiaomuyu1215 <48826001+xiaoxiaomuyu1215@users.noreply.github.com>
2020-07-31 17:43:01 +0800

    Add files via upload

--- new/rgb_driver.v
+++ new/rgb_driver.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2020/07/27 13:10:33
+// Create Date: 2020/07/27 15:26:50
 // Design Name: 
 // Module Name: rgb_driver
 // Project Name: 
@@ -20,39 +20,56 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+
 module rgb_driver(
     input clk_10MHz,
-    input H_sync,
+    input i_rst,
+    input [10:0]set_x,
+    input [9:0]set_y,
     input [23:0]rgb_data,
     output rgb_led
     );
     reg [7:0]R_In=0;
     reg [7:0]G_In=0;
     reg [7:0]B_In=0;
-    reg [15:0]h_cnt=0;
+    reg [6:0]cnt1=0;
+    reg [3:0]cnt=0;
+    
     Driver_SK6805_0 rgb0(
     .R_In1(R_In),
     .G_In1(G_In),
     .B_In1(B_In),
-    .R_In2(R_In),
-    .G_In2(G_In),
-    .B_In2(B_In),
+    .R_In2(0),
+    .G_In2(0),
+    .B_In2(0),
     .clk_10MHz(clk_10MHz),
-    .Rst(1),
+    .Rst(i_rst),
     .LED_IO(rgb_led)
     );
-    always@(posedge H_sync)
+    always@(*)
         begin 
-            if(h_cnt==115|h_cnt==295|h_cnt==475|h_cnt==655)
+            if(set_x==640&set_y==360)
                 begin
-                    R_In<=rgb_data[23:16];
-                    G_In<=rgb_data[15:8];
-                    B_In<=rgb_data[7:0];
-                    h_cnt<=h_cnt+1;
+                    cnt1<=cnt1+1;
+                    if(cnt1==120)
+                        begin
+                            R_In<=rgb_data[23:16];
+                            B_In<=rgb_data[15:8];
+                            G_In<=rgb_data[7:0];
+                            cnt1<=0;
+                        end
+                    else
+                        begin
+                            R_In<=R_In;
+                            B_In<=B_In;
+                            G_In<=G_In;
+                        end
                 end
-            else if(h_cnt==749)
-                h_cnt<=0;
             else 
-                h_cnt<=h_cnt+1;
+                begin
+                    R_In<=R_In;
+                    B_In<=B_In;
+                    G_In<=G_In;
+                end
         end
 endmodule
